e5162b0d62eb0dcd4bb3a78e97c60a68ef657330
Michael Zhang <xuanmclane@gmail.com>
2017-09-28 17:02:18 -0400

    to be continue

---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { CardSection } from './common';
+import { connect } from 'react-redux'
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import * as actions from '../actions'
+
+class ListItem extends Component {
+	render () {
+		const { titleStyle } =styles
+		const { id, title } =this.props.library
+		console.log(this.props)
+		return (
+			<TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+				<View>
+					<CardSection>
+						<Text style={titleStyle}>
+							{title}
+						</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
+		);
+	}
+}
+
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+}
+
+export default connect(null, actions)(ListItem);
